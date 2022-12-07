026c9f6a6658fa6c5cc0354a6cd155c9275c0a81
Wardedmedusa <120023031+Wardedmedusa@users.noreply.github.com>
2022-12-07 16:18:23 +0000

    Commit

---
+++ textfeedback.js
@@ -0,0 +1,70 @@
+
+function myFunction() { 
+
+    var x = document.getElementById("form1"); 
+
+    var text = ""; 
+
+    var i; 
+
+    for (i = 0; i < x.length ;i++) { 
+
+        data = x.elements[i].value; 
+
+if (data!="This submits the data to another page") { 
+
+text += "Your feedback text was entered as " + data + "."; 
+
+} 
+
+    } 
+
+    document.getElementById("output_here").innerHTML = text; 
+
+} 
+function myFunction2() { 
+
+    var x = document.getElementById("form2"); 
+
+    var text = ""; 
+
+    var i; 
+
+    for (i = 0; i < x.length ;i++) { 
+
+        data = x.elements[i].value; 
+
+if (data!="This submits the data to another page") { 
+
+text += "Your email text was entered as " + data + "."; 
+
+} 
+
+    } 
+
+    document.getElementById("output_here2").innerHTML = text; 
+
+}  
+function myFunction3() { 
+
+    var x = document.getElementById("form3"); 
+
+    var text = ""; 
+
+    var i; 
+
+    for (i = 0; i < x.length ;i++) { 
+
+        data = x.elements[i].value; 
+
+if (data!="This submits the data to another page") { 
+
+text += "Your date text was entered as " + data + "."; 
+
+} 
+
+    } 
+
+    document.getElementById("output_here3").innerHTML = text; 
+
+} 
