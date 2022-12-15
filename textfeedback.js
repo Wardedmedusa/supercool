83979a88e4b6037fae4faa921d7ae66a414a50bf
Wardedmedusa <120023031+Wardedmedusa@users.noreply.github.com>
2022-12-15 21:47:16 +0000

    Commit 15/12/22 #5

--- textfeedback.js
+++ textfeedback.js
@@ -66,7 +66,6 @@ text += "Your date text was entered as " + data + ".";
     } 
 
     document.getElementById("output_here3").innerHTML = text; 
-
 } 
 function myFunction4() { 
 
@@ -78,16 +77,85 @@ function myFunction4() {
 
     for (i = 0; i < x.length ;i++) { 
 
-        data = x.elements[i].value; 
+        forename = x.elements[i].value; 
 
-if (data!="This submits the data to another page") { 
+if (forename!="This submits the data to another page") { 
 
-text += "Your date text was entered as " + data + "."; 
+text += "Your date text was entered as " + forename + "."; 
 
 } 
 
     } 
 
     document.getElementById("output_here4").innerHTML = text; 
+
+}
+function myFunction5() { 
+
+    var x = document.getElementById("form5"); 
+
+    var text = ""; 
+
+    var i; 
+
+    for (i = 0; i < x.length ;i++) { 
+
+        surname = x.elements[i].value; 
+
+if (surname!="This submits the data to another page") { 
+
+text += "Your date text was entered as " + surname + "."; 
+
+} 
+
+    } 
+
+    document.getElementById("output_here6").innerHTML = text; 
+
+} 
+function myFunction6() { 
+
+    var x = document.getElementById("form6"); 
+
+    var text = ""; 
+
+    var i; 
+
+    for (i = 0; i < x.length ;i++) { 
+
+        number = x.elements[i].value; 
+
+if (number!="This submits the data to another page") { 
+
+text += "Your date text was entered as " + number + "."; 
+
+} 
+
+    } 
+
+    document.getElementById("output_here6").innerHTML = text; 
+
+} 
+function myFunction7() { 
+
+    var x = document.getElementById("form7"); 
+
+    var text = ""; 
+
+    var i; 
+
+    for (i = 0; i < x.length ;i++) { 
+
+        pin = x.elements[i].value; 
+
+if (pin!="This submits the data to another page") { 
+
+text += "Your date text was entered as " + pin + "."; 
+
+} 
+
+    } 
+
+    document.getElementById("output_here7").innerHTML = text; 
 
 } 
